a170be200214a081e119f7ef5547dad6d4834bdc
Anno88 <31775315+Anno88@users.noreply.github.com>
2018-02-11 01:35:18 +0100

    Create library.js

---
+++ library.js
@@ -0,0 +1,11 @@
+var test = getAttributesByClassName("data-value", "theClass");
+
+function getAttributesByClassName (attribute, className){
+	var cName = document.getElementsByClassName(className);
+	var attr = [];
+	for(i=0;i<cName.length;i++){
+		attr.push(className[i].getAttribute(attribute));
+	}
+	return attr;
+}
+console.log(test);
