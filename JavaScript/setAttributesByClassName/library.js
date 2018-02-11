25ab5ec30f7082381b3a57af10dc3d82aca81560
ghost <ghost@localhost.localdomain>
2018-02-11 18:48:27 +0100

    Update.

--- JavaScript/setAttributesByClassName/library.js
+++ JavaScript/setAttributesByClassName/library.js
@@ -1,7 +1,7 @@
-var test = setAttributesByClassName("value","clicked","theClass");
+setAttributesByClassName("value","clicked","theClass");
 
-//Function go get all attributes with specific name from a specific class
-//Call function and insert attribute name and class name separated by a comma as demonstrated in the test above
+//Function go set attribute values to all attributes with specific name in elements with a specific class
+//Call function and insert attribute name, attribute value and class name separated by a comma as demonstrated in the test above
 function setAttributesByClassName (attributeName,attributeValue,className){
 	var cName = document.getElementsByClassName(className);
 	var loopLenght = cName.length-1;
